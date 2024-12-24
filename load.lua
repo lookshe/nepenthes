b74006334953ca981f74c5c4aaa83a7aa6433f6e
aaron <aaron.com>
2024-12-24 19:52:47 +0000

    Embed the schema directly into the load process, to simplify bootstrapping.

--- load.lua
+++ load.lua
@@ -2,7 +2,43 @@
 
 require 'luarocks.loader'
 local sqltable = require 'sqltable'
+local dbgen = require 'dbgen'
 
+
+local schema = {
+	--[[
+	--PRAGMA journal_mode = wal;
+	--]]
+
+	--[[
+	--PRAGMA wal_autocheckpoint(100);
+	--]]
+
+	[[
+	create table tokens (
+		id integer primary key autoincrement,
+		oken varchar(255) not null, -- this weird name will make sense later
+		unique( oken )
+	);
+	]],
+
+	[[
+	insert into tokens( id, oken ) values ( 1, '' );
+	]],
+
+	[[
+	create table token_sequence (
+		id integer primary key autoincrement,
+		prev_1 integer not null references tokens ( id ),
+		prev_2 integer not null references tokens ( id ),
+		next_id integer not null references tokens ( id )
+	);
+	]],
+
+	[[
+	create index seq on token_sequence ( prev_1, prev_2 );
+	]]
+}
 
 local function allwords ( f )
 	local line = f:read("*line")    -- current line
@@ -30,6 +66,9 @@ local sql = sqltable.connect {
 	type = 'SQLite3',
 	name = arg[2]
 }
+
+dbgen.setup( 'SQLite3', sql, schema )
+sql:reset()
 
 local tokens = assert(sql:open_table
 	{
