d210626f5505d08c4240ebeabc565c2a0e0fb8cf
aaron <aaron.com>
2024-12-31 21:45:29 +0000

    Loading script improvements.

--- load.sh
+++ load.sh
@@ -14,27 +14,39 @@ url=$1; shift
 
 size_lines=`wc -l $text`
 size_sent=0
+skip=0
+
+#
+# Third argument: Skip ahead (x) lines, if training got interrupted
+#
+if [ \! -z "$1" ]; then
+	skip=$1
+fi
 
 do_post()
 {
 	size_sent=`expr $size + $size_sent`
-	echo -n "date \"+%H:%m\" sending $size_sent of $size_lines: "
+	echo -n "`date \"+%H:%m\"` sending $size_sent of $size_lines: "
 	curl -XPOST -H'Content-type: text/plain' -d "$send" $url || exit 1
 	
-	echo "done."
+	echo "  done."
 }
-
 
 size=0
 send=""
 while read line; do
 
-	send="$send $line"
-	size=`expr $size + 1`
-	if [ $size -ge 100 ]; then
-		do_post
-		size=0
-		body=""
+	if [ $skip -gt 0 ]; then
+		skip=`expr $skip - 1`
+		size_sent=`expr $size_sent + 1`
+	else
+		send="$send $line"
+		size=`expr $size + 1`
+		if [ $size -ge 10 ]; then
+			do_post
+			size=0
+			body=""
+		fi
 	fi
 
 done < $text
