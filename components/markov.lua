ca9ec27e5c9a87ca71274baceabddab3a4b4bd74
aaron <aaron.com>
2025-01-09 04:55:08 +0000

    Complicate the database further, as a performance optimization.

--- components/markov.lua
+++ components/markov.lua
@@ -7,14 +7,6 @@ local dbgen = require 'components.dbgen'
 
 
 local schema = {
-	--[[
-	--PRAGMA journal_mode = wal;
-	--]]
-
-	--[[
-	--PRAGMA wal_autocheckpoint(100);
-	--]]
-
 	[[
 	create table tokens (
 		id integer primary key autoincrement,
@@ -32,12 +24,57 @@ local schema = {
 		id integer primary key autoincrement,
 		prev_1 integer not null references tokens ( id ),
 		prev_2 integer not null references tokens ( id ),
-		next_id integer not null references tokens ( id )
+		unique( prev_1, prev_2 )
 	);
 	]],
 
 	[[
-	create index seq on token_sequence ( prev_1, prev_2 );
+	create table token_next (
+		prev_id integer not null references token_sequence ( id ),
+		token_id integer not null references tokens ( id )
+	);
+	]],
+
+	[[
+	create view token_seq_next as
+		select	ts.id as id,
+				t1.oken as prev_1,
+				t2.oken as prev_2,
+				t3.oken as next_t
+
+			from token_sequence as ts
+			join tokens as t1
+				on ts.prev_1 = t1.id
+			join tokens as t2
+				on ts.prev_2 = t2.id
+			join token_next as tn
+				on tn.prev_id = ts.id
+			join tokens as t3
+				on tn.token_id = t3.id;
+	]],
+
+	[[
+	create trigger if not exists token_seq_next_insert
+		instead of insert on token_seq_next
+		begin
+
+			insert or ignore into tokens ( oken )
+				values	( NEW.prev_1 ),
+						( NEW.prev_2 ),
+						( NEW.next_t );
+
+			insert or ignore into token_sequence ( prev_1, prev_2 )
+				select	( select id from tokens where oken = NEW.prev_1 ),
+						( select id from tokens where oken = NEW.prev_2 );
+
+			insert into token_next ( prev_id, token_id )
+				select distinct
+						( select id from token_sequence
+							where prev_1 = ( select id from tokens where oken = NEW.prev_1 )
+								and prev_2 = ( select id from tokens where oken = NEW.prev_2 )
+							),
+						( select id from tokens where oken = NEW.next_t );
+		end;
 	]]
 }
 
@@ -49,21 +86,9 @@ local sql = sqltable.connect {
 dbgen.setup( 'SQLite3', sql, schema )
 sql:reset()
 
-local tokens = assert(sql:open_table
-	{
-		name = 'tokens',
-		key = 'id'
-	})
-
-local load_tokens = assert(sql:open_table
-	{
-		name = 'tokens',
-		key = 'oken'
-	})
-
 local seq = assert(sql:open_table
 	{
-		name = 'token_sequence',
+		name = 'token_seq_next',
 		key = 'id'
 	})
 
@@ -82,33 +107,16 @@ local _M = {}
 --
 function _M.train( corpus )
 
-	-- cache of seen words by ID.
-	local seen = {
-		[''] = 1
-	}
-
 	local count = 0
 	local prev1 = ''
 	local prev2 = ''
 
 	for word in corpus:gmatch("%S+") do
-		-- insert word, if needed
-		if not seen[word] then
-			local ct = load_tokens[ word ]
-
-			if not ct then
-				load_tokens[ sql.next ] = { oken = word }
-				ct = load_tokens[ word ]
-			end
-
-			seen[word] = ct.id
-		end
-
 		-- insert token sequence
 		seq[ sql.next ] = {
-			prev_1 = seen[prev1],
-			prev_2 = seen[prev2],
-			next_id = seen[word]
+			prev_1 = prev1,
+			prev_2 = prev2,
+			next_t = word
 		}
 
 		-- Give web requests a chance
@@ -140,21 +148,32 @@ function _M.babble( rnd )
 
 	local len = 0
 	local prev1, prev2, cur
-	local start = seq[ rnd:between( seq_size, 1 ) ]
 	local ret = {}
 
+	local first = rnd:between( seq_size, 1 )
+	local start = sql.iclone( seq, "1 = 1 order by id, next_t desc limit $1, 1;", first )
 	local size = rnd:between( config.markov_max or 300, config.markov_min or 100 )
 
-	prev2 = start.prev_2
-	cur = start.next_id
+	prev2 = start[1].prev_2
+	cur = start[1].next_t
 
 	repeat
 		prev1 = prev2
 		prev2 = cur
 
-		local opts = sql.iclone( seq, 'prev_1 = $1 and prev_2 = $2',
-			prev1, prev2
+		local opts = {}
+		sql:exec( 'select next_t from token_seq_next where prev_1 = $1 and prev_2 = $2',
+			{ prev1, prev2 },
+			function( conn, statement )	-- luacheck: ignore 212
+				local row = statement:fetch()
+				while row do
+					opts[ #opts + 1 ] = row[1]
+					row = statement:fetch()
+				end
+			end
 		)
+
+		for i, v in ipairs(opts) do print(i, v) end
 
 		-- something went wrong
 		if not opts then
@@ -166,8 +185,8 @@ function _M.babble( rnd )
 			which = rnd:between( #opts, 1 )
 		end
 
-		cur = opts[ which ].next_id
-		ret[ #ret + 1 ] = tokens[ cur ].oken
+		cur = opts[ which ]
+		ret[ #ret + 1 ] = cur
 		len = len + 1
 	until len >= size
 
