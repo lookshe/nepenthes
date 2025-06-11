766159ba78085dc731d287209868b175fbc4955d
aaron <aaron.com>
2025-06-11 23:57:03 +0000

    Clear Luacheck.

--- components/send.lua
+++ components/send.lua
@@ -15,11 +15,11 @@ end
 
 local _M = {}
 
-local function merge( a, b )	
+local function merge( a, b )
 	for i in ipairs(b) do
 		a[ #a + 1 ] = b[i]
 	end
-	
+
 	return a
 end
 
@@ -34,26 +34,26 @@ end
 local function split( delay, bytes )
 
 	if bytes < 2 then
-		return { { 
-			bytes = bytes, 
+		return { {
+			bytes = bytes,
 			delay = delay
 		} }
 	end
-	
+
 	if delay < 500 then
-		return { { 
-			bytes = bytes, 
+		return { {
+			bytes = bytes,
 			delay = delay
 		} }
 	end
-	
+
 	local left_bytes
 	if bytes == 2 then
 		left_bytes = 1
 	else
 		left_bytes = rand.uniform( bytes - 1 ) + 1
 	end
-	
+
 	local right_bytes = bytes - left_bytes
 
 	local left_delay = rand.uniform( delay - 1 ) + 1
@@ -61,11 +61,11 @@ local function split( delay, bytes )
 
 	local left = split( left_delay, left_bytes )
 	local right = split( right_delay, right_bytes )
-	
+
 	return merge(left, right)
-	
+
 end
-	
+
 ---
 -- Generate a unique pattern of when and how much data is sent.
 --
@@ -92,7 +92,7 @@ function _M.generate_pattern( delay, bytes )
 	-- Fire the recursively splitting packet randomizer.
 	--
 	local ret = split( delay * 1000, bytes )
-	
+
 	--
 	-- Occasionally, part of the tree the above creates doesn't have
 	-- enough bytes for the amount of delay it needs to split up - let's
@@ -100,21 +100,21 @@ function _M.generate_pattern( delay, bytes )
 	--
 	local largest = 1
 	local to_fix = {}
-	
+
 	for i, v in ipairs(ret) do
 		if v.bytes > ret[largest].bytes then
 			largest = i
 		end
-		
+
 		if v.delay > 500 then
 			to_fix[ #to_fix + 1 ] = i
 		end
 	end
-	
+
 	if #to_fix > 0 then
 		local new_delay = ret[largest].delay
 		local new_bytes = ret[largest].bytes
-	
+
 		for i, fix in ipairs( to_fix ) do
 			new_delay = new_delay + ret[fix].delay
 			new_bytes = new_bytes + ret[fix].bytes
@@ -127,10 +127,10 @@ function _M.generate_pattern( delay, bytes )
 			table.remove(ret, fix)
 		end
 
-		
+
 		ret = merge( ret, split( new_delay, new_bytes ) )
 	end
-		
+
 	local delayout = 0
 	local bytesout = 0
 	for i, v in ipairs( ret ) do
@@ -138,7 +138,7 @@ function _M.generate_pattern( delay, bytes )
 		bytesout = bytesout + v.bytes
 		v.delay = v.delay / 1000
 	end
-	
+
 	return ret
 
 end
--- tests/send.lua
+++ tests/send.lua
@@ -18,58 +18,58 @@ describe("Send-Request-Output Module", function()
 			15, 1200,
 			46, 700
 		}
-		
+
 		repeat
-		
+
 			local delay = table.remove(tests, 1)
 			local bytes = table.remove(tests, 1)
-		
+
 			local pattern = send.generate_pattern( delay, bytes )
 			assert.is_table(pattern)
-			
+
 			--
 			-- We can't test actual values here, as we aren't using
 			-- a deterministic random number generator in this case,
 			-- help avoid detection.
 			--
-			-- *Well, technically it is deterministic, but a 
-			-- cryptographically secure generator is effectively 
-			-- nondeterministic looking without knowing it's internal 
+			-- *Well, technically it is deterministic, but a
+			-- cryptographically secure generator is effectively
+			-- nondeterministic looking without knowing it's internal
 			-- state. I think.
 			--
 			-- So instead, the test proves it meets the stochastic
 			-- effects desired.
 			--
-			
-			-- the smallest requested delay, 8 seconds, implies 
+
+			-- the smallest requested delay, 8 seconds, implies
 			-- typically sixteen outbound packets.
 			assert.is_true( #pattern > 8 )
-			
+
 			--
 			-- Prove the individual packets look nice.
 			--
 			local bytes_total = 0
 			local delay_total = 0
-			
+
 			for i, packet in ipairs( pattern ) do
 				assert.is_table( packet )
 				assert.is_number( packet.delay )
 				assert.is_number( packet.bytes )
-				
+
 				assert.is_true( packet.delay > 0 )
 				--assert.is_true( packet.delay <= 1 )
 				assert.is_true( packet.bytes > 0 )
 				assert.is_true( packet.bytes < ( bytes // 1.5 ) )
-				
+
 				bytes_total = bytes_total + packet.bytes
 				delay_total = delay_total + packet.delay
 			end
-			
+
 			--print('##', bytes_total, delay_total)
 			assert.is_equal( bytes, bytes_total )
 			assert.is_true( delay > delay_total - 1 )
 			assert.is_true( delay < delay_total + 2 )
-			
+
 		until #tests == 0
 
 	end)
