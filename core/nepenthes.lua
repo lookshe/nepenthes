cab5cb3db909774dd237e41aae55a4e01f89b8b7
aaron <aaron.com>
2026-09-01 20:36:08 +0000

    Rip out front-end logic to it's own module for testability.

--- core/nepenthes.lua
+++ core/nepenthes.lua
@@ -1,21 +1,17 @@
 #!/usr/bin/env lua5.4
 
-local cqueues = require 'cqueues'
 local json = require 'dkjson'
 
 local perihelion = require 'perihelion'
-local output = require 'daemonparts.output'
-local corewait = require 'daemonparts.corewait'
 
 local stats = require 'components.stats'
-local stutter = require 'components.stutter'
 local silo = require 'components.silo'
+local frontend = require 'components.frontend'
 
 
 silo.setup()
 
 local app = perihelion.new()
-
 
 app:get "/stats/silo/(%S+)/addresses" {
 	function ( web, silo_filter )
@@ -102,173 +98,14 @@ app:get "/stats" {
 }
 
 
-
-local function checkpoint( times, name )
-	times[ #times + 1 ] = {
-		name = name,
-		at = cqueues.monotime()
-	}
-end
-
-local function log_checkpoints( times, send_delay, logged_silo )
-
-	local parts = {}
-
-	for i, cp in ipairs( times ) do	-- luacheck: ignore 213
-		if cp.name ~= 'start' then
-			parts[ #parts + 1 ] = string.format('%s: %f', cp.name, cp.at - times[1].at)
-		end
-	end
-
-	if send_delay then
-		parts[ #parts + 1 ] = string.format('send_delay: %f', send_delay)
-	end
-
-	if logged_silo then
-		parts[ #parts + 1 ] = string.format('silo: %s', logged_silo)
-	end
-
-	output.info("req len: " .. table.concat( parts, ', ' ))
-
-end
-
-
-local function log_misc_hit( web, req, code, delay )
-
-	local logged = stats.new_entry {
-		address = web.REMOTE_ADDR,
-		uri = web.PATH_INFO,
-		agent = web.HTTP_X_USER_AGENT,
-		silo = req.silo,
-		bytes_generated = 0,
-		when = os.time(),
-		response = code,
-		planned_delay = delay,
-		cpu = 0,
-		complete = true
-	}
-
-	logged:record( 0, delay )
-	logged:mark_complete()
-
-end
-
-local function log_bogon( web, req, delay )
-	log_misc_hit( web, req, 404, delay )
-end
-
-local function log_redirect( web, req, delay )
-	log_misc_hit( web, req, 302, delay )
-end
-
-local function log_head( web, req, delay )
-	log_misc_hit( web, req, 200, delay )
-end
-
-
----
--- Common code for both HEAD and GET requests.
---
-local function request_preprocess( web )
-
-	local ts = {}
-	checkpoint( ts, 'start' )
-
-	local req = silo.new_request( web.HTTP_X_SILO, web.PATH_INFO )
-
-	if req:is_bogon() then
-		output.notice("Bogon URL:", web.REMOTE_ADDR, "asked for", web.PATH_INFO)
-		local pause = req:header_wait()
-		corewait.poll( pause )
-		log_bogon( web, req, pause )
-		return web:notfound("Nothing exists at this URL")
-	end
-
-	local is_redirect, location = req:is_redirect()
-	if is_redirect then
-		local pause = req:header_wait()
-		corewait.poll( pause )
-		log_redirect( web, req, pause )
-		return web:redirect( location )
-	end
-
-	return {
-		ts  = ts,
-		req = req
-	}
-
-end
-
-
----
--- Some crawlers HEAD every url before GET. Since it will always result
--- in a document (request has already cleared the bogon check during
--- setup), don't do anything.
---
 app:head "/(.*)" {
-	request_preprocess,
-	function( web )
-		local req = web.vars.req
-
-		local pause = req:header_wait()
-		corewait.poll( pause )
-		log_head( web, req, pause )
-		web.headers['content-type'] = 'text/html; charset=UTF-8'
-		return web:ok("")
-	end
+	frontend.preprocess,
+	frontend.HEAD
 }
 
----
--- Actual tarpitting happens here.
---
 app:get "/(.*)" {
-	request_preprocess,
-	function( web )
-
-		local req = web.vars.req
-		local ts = web.vars.ts
-
-		checkpoint( ts, 'preprocess' )
-		req:load_markov()
-		req:set_booleans()
-		checkpoint( ts, 'markov' )
-		local page = req:render()
-		local wait = req:send_delay()
-		checkpoint( ts, 'rendering' )
-
-		local siloname
-		if silo.count() > 1 then
-			siloname = req.silo
-		end
-
-		log_checkpoints( ts, wait, siloname )
-
-		local time_spent = ts[ #ts ].at - ts[1].at
-		local logged = stats.new_entry {
-			address = web.REMOTE_ADDR,
-			uri = web.PATH_INFO,
-			agent = web.HTTP_X_USER_AGENT,
-			silo = req.silo,
-			bytes_generated = #page,
-			when = os.time(),
-			response = 200,
-			planned_delay = wait,
-			cpu = time_spent
-		}
-
-		web.statistic_log = logged
-		web.headers['content-type'] = 'text/html; charset=UTF-8'
-
-		if req.zero_delay then
-			return '200 OK', web.headers,
-				stutter.zero_delay_iterator( page, logged )
-		end
-
-		return '200 OK', web.headers, stutter.delay_iterator (
-				page, logged,
-				stutter.generate_pattern( wait, #page )
-			)
-	end
+	frontend.preprocess,
+	frontend.GET
 }
 
 return app
---
+++ tests/frontend.lua
@@ -0,0 +1,148 @@
+#!/usr/bin/env lua5.4
+
+require 'luarocks.loader'
+pcall(require, 'luacov')
+
+local cqueues = require 'cqueues'
+
+local config = require 'components.config'
+local silo = require 'components.silo'
+
+--
+-- Monkey patch this to make it identical from test run to test run.
+--
+local seed = require 'components.seed'
+seed.get = function()
+	return 'ec708cffc8c154521ced80639449576ff8bd356060eeb20aecfc76e45ec80bbc'
+end
+
+
+local frontend = require 'components.frontend'
+
+
+local function mock_request( req_type, uri, query, name, post_data )
+
+	local ret = {}
+
+	ret.GET = {}
+	ret.POST = {}
+	ret.REQUEST_METHOD = req_type
+	ret.QUERY_STRING = query or ""
+	ret.PATH_INFO = uri or "/"
+	ret.SCRIPT_NAME = name or "nepenthes.lua"
+	ret.CONTENT_TYPE = ""
+	ret.CONTENT_LENGTH = ""
+	ret.REMOTE_ADDR = "127.0.0.1"
+	ret.SESSION = {}
+	ret.input = nil
+	ret.headers = {}
+	ret.vars = {}
+
+	function ret.ok( web, response_body )
+		local out = ""
+
+		if type(response_body) == 'string' then
+			out = response_body
+		elseif type(response_body) == 'table' then
+			out = table.concat(response_body, " ")
+		elseif type(response_body) == 'function' then
+			repeat
+				local x = response_body()
+				out = out .. tostring(x)
+			until not out
+		else
+			error("unknown returned type")
+		end
+
+		return '200 OK', web.headers, out
+	end
+
+	if post_data then
+		ret.input = assert(io.open(post_data, "r"))
+		ret.CONTENT_LENGTH = #(ret.input:read("*all"))
+		ret.input:seek("set", 0)
+	end
+
+	return ret
+
+end
+
+
+require 'busted.runner'()
+describe("Frontend Routes", function()
+
+	--local app = frontend.new()
+
+	setup(function()
+		config.silos = {
+			{
+				name = 'default',
+				corpus = './tests/share/wiki-markov.txt',
+				wordlist = './tests/share/words.txt',
+				template = 'default',
+				min_wait = 1,
+				max_wait = 5,
+				header_min_wait = 1,
+				header_max_wait = 1
+			}
+		}
+
+		silo.setup()
+	end)
+
+
+	it("Handles GET", function()
+
+		local test_start = cqueues.monotime()
+		local web = mock_request( 'GET', '/' )
+
+		local vars = frontend.preprocess( web )
+		assert.is_table( vars )
+		web.vars = vars
+
+		local status, headers, out = frontend.GET( web )
+		assert.is_function( out )
+		assert.is_table( headers )
+		assert.is_equal( '200 OK', status )
+
+		local s = ''
+		for val in out do
+			s = s .. val
+		end
+
+		assert.is_string(s)
+		assert.is_equal( 1647, #s )
+		local test_end = cqueues.monotime()
+		local duration = test_end - test_start
+
+		-- 1 second more than config, allow some processing overhead
+		assert.is_true( duration <= 6 )
+		assert.is_true( duration >= 1 )
+
+	end)
+
+
+	it("Handles HEAD", function()
+
+		local test_start = cqueues.monotime()
+		local web = mock_request( 'HEAD', '/' )
+
+		local vars = frontend.preprocess( web )
+		assert.is_table( vars )
+		web.vars = vars
+
+		local status, headers, out = frontend.HEAD( web )
+		assert.is_equal( '', out )
+		assert.is_table( headers )
+		assert.is_equal( '200 OK', status )
+
+		local test_end = cqueues.monotime()
+		local duration = test_end - test_start
+
+		-- 1 second more than config, allow some processing overhead
+		assert.is_true( duration <= 2 )
+		assert.is_true( duration >= 1 )
+
+	end)
+
+end)
